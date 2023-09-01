e8fc41260ad31f625e216107b08ac024e8f3b46e
vnvillarrealv <vnvillarrealv@gmail.com>
2023-09-01 00:31:09 -0500

    corregido signup

--- static/js/signup.js
+++ static/js/signup.js
@@ -8,7 +8,28 @@
     // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
+
+        let password = document.getElementById('floatingPassword');
+        let password_ok = validar_password(password.value);        
+        if (password_ok) {
+          password.classList.remove("is-invalid");
+          password.classList.add("is-valid");
+        }else {
+          password.classList.remove("is-valid");
+          password.classList.add("is-invalid");
+        }
+
+        let password2 = document.getElementById('floatingPassword2');
+        if (password.value == password2.value) {
+          password2.classList.remove("is-invalid");
+          password2.classList.add("is-valid");
+        }else {
+          password_ok = false;
+          password2.classList.remove("is-valid");
+          password2.classList.add("is-invalid");
+        }
+
+        if (!form.checkValidity() || !password_ok) {
           event.preventDefault()
           event.stopPropagation()
         }
@@ -18,12 +39,52 @@
     })
 })()
 
-const sign_up = (csrf_token) => {
+let cumpleLongitud, cumpleMayus, cumpleNum, cumpleCarEspec;    
+const longitud = 8;
+const caracteresEspeciales = "!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?`~";
+const numeros = "0123456789";
+
+const validar_password = (password) => {
+  if (password.length < longitud) {
+    cumpleLongitud = false;
+  } else if (password.length > 7) {
+    cumpleLongitud = true;
+  }
+
+  if (password.toLowerCase() == password) {
+    cumpleMayus = false;
+  } else {
+    cumpleMayus = true;
+  }
+
+  cumpleNum = false;
+  for (let i = 0; i < password.length; i++) {
+      for (let j = 0; j < numeros.length; j++) {
+          if (password[i] == numeros[j]) {
+            cumpleNum = true;
+          }
+      }
+  }
+
+  cumpleCarEspec = false;
+  for (let i = 0; i < password.length; i++) {
+      for (let j = 0; j < caracteresEspeciales.length; j++) {
+          if (password[i] == caracteresEspeciales[j]) {
+            cumpleCarEspec = true;
+          }
+      }
+  }
+
+  return cumpleLongitud == true && cumpleMayus  == true && cumpleNum  == true && cumpleCarEspec == true;
+}
+
+const sign_up = () => {
     let email = document.getElementById('floatingEmailInput').value;
     let nombre = document.getElementById('floatingNombreInput').value;
     let apellido = document.getElementById('floatingApellidoInput').value;    
     let password = document.getElementById('floatingPassword').value;
-    let password2 = document.getElementById('floatingPassword2').value;
+    
+    let csrf_token = Cookies.get('csrftoken');
 
     axios.post('/signup/',
       {'nombre': nombre, 'apellido': apellido, 'email': email, 'password': password},
